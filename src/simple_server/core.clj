e612f2ef04f7bec3edd53dbf3aa7d24b9486dc6d
quinn_jh <quinnjarvisholland@gmail.com>
2023-01-11 23:22:54 -0500

    Update core.clj

--- src/simple_server/core.clj
+++ src/simple_server/core.clj
@@ -6,6 +6,18 @@
    :headers {"Content-Type" "text/html"}
    :body "<h1> HI !!!</h1>"})
 
+(defn image-handler []
+  (let [{:keys [status body]} (http/get "https://some.image.url/image.jpg"  {:as :byte-array})]
+    (if (= 200 status)
+      {:status 200
+       :headers {"Content-Type" "image/jpeg"}
+       :body body}
+      {:status 404
+       :body "Image not found"})))
+You'll need to route requests to this handler using Compojure:
+(defroutes app-routes
+  (GET "/image" [] (image-handler)))
+
 (defn create-server []
   (s/run-server handler {:port 8080}))
 
